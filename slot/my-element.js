038d79f1fdebbc9b79654cd7dcfb649f8b1244e8
angelDariaux <angeldariaux@gmail.com>
2022-01-31 22:21:05 -0600

    usado <slot> para agregar el texto dentro de la etiqueta my-element

---
+++ slot/my-element.js
@@ -0,0 +1,42 @@
+class myElement extends HTMLElement { // Con extends HTMLElement comenzamos los elementos personalizados
+  constructor () { 
+    super();        // ? La palabra clave super es usada para acceder y llamar funciones del padre de un objeto.
+    this.attachShadow({ mode: "open"}); // ? Asi abrimos el shadown dom (shadow-root (open))
+  }
+  getTemplate () { 
+    const template = document.createElement('template'); //! slot agrega el texto que esta en la etiqueta my-element
+    template.innerHTML = ` 
+      <section>
+        <h2>
+          <slot></slot>
+        </h2>
+      </section>
+      ${this.getStyles()}
+    `;
+    return template;
+  }
+  getStyles () { 
+    return `
+    <style> 
+      h2 { 
+        color: red;
+      }
+    </style>  
+    `;
+  }
+  render () { // * es importate agregar esto this.shadowRoot.appendChild para que al final renderice todo lo que escribimos
+   this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true)); // ! Asi clonamos el nodo de getTemplate, true se usa para que clone todos los elementos del getTemplate
+  }
+  connectedCallback () { // ? para visualizar en la etiqueta p, vamos a usar el connected Callback.
+    this.render();
+  }          
+}
+
+
+
+
+
+customElements.define('my-element', myElement) // * Asi es como se crea la etiqueta HTML del web component
+
+
+
